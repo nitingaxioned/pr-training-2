af09a95ace20831bfe5b1b58366daef54eb82996
Nitin <niting@axioned.com>
2022-03-08 14:36:17 +0530

    single-job fixed.

---
+++ wp-content/themes/opera/single-job.php
@@ -0,0 +1,29 @@
+<?php 
+get_header();
+$title = get_the_title();
+$job_role = get_field('your_role');
+$experience = get_field('experience');
+?>
+<!--main section start-->
+<main>
+    <div class="wrapper">
+        <?php if($title)  {?>
+            <h2><?php echo $title; ?></h2>
+        <?php }
+        if($job_role) { ?>
+            <div>
+                <h3>Your Role</h3>
+                <?php echo $job_role; ?>
+            </div>
+        <?php } 
+        if($experience) { ?>
+            <div>
+                <h3>Your Background/Experience</h3>
+                <?php echo $experience; ?>
+            </div>
+        <?php } ?>
+    </div>
+</main>
+<!--main section end-->
+<?php
+get_footer(); 
