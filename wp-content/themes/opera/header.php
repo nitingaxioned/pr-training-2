0879b4519823d3f929a87a6df28170fd56c34fb0
Nitin <niting@axioned.com>
2022-03-10 13:43:07 +0530

    header fixed

--- wp-content/themes/opera/header.php
+++ wp-content/themes/opera/header.php
@@ -3,6 +3,9 @@
 <head>
   <?php wp_head(); ?>
   <meta charset="utf-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;1,200&display=swap" rel="stylesheet">
 </head>
 <body <?php body_class(); ?>>
   <!--container start-->
